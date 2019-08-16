b1a861edcb9ae3eebba5c1c38af9e931a7ab4743
Adam Elders <Ramor.1990@gmail.com>
2019-08-15 23:30:47 -0400

    Add ng-bootstrap module

--- Trinity CMS/trinity-cms/src/app/app.module.ts
+++ Trinity CMS/trinity-cms/src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
