796ea69259a37ebc0d284c34ddb28d82eb797e9f
Adam Elders <Ramor.1990@gmail.com>
2020-02-07 01:00:21 -0500

    Adding menu item component WIP

--- Trinity CMS/trinity-cms/src/app/app.module.ts
+++ Trinity CMS/trinity-cms/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { AccountRegisterComponent } from './account-register/account-register.component';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuItemComponent } from './menu-item/menu-item.component';
 
 const appRoutes: Routes = [
   { path: 'register', component: AccountRegisterComponent }
 ];
 
 @NgModule({
-  declarations: [AppComponent, MainMenuComponent, AccountRegisterComponent],
+  declarations: [AppComponent, MainMenuComponent, AccountRegisterComponent, MenuItemComponent],
   imports: [
     RouterModule.forRoot(appRoutes, { enableTracing: true }),
     BrowserModule,
